ede7ba7ab7d1c848d6933cea5cf01bdc21a99804
Alejandro Vivas <alejandrovivash5@gmail.com>
2025-08-21 14:51:19 -0400

    test

--- main/main.c
+++ main/main.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "driver/gpio.h"
 
-TaskHandle_t myTaskHandle = NULL; 
-TaskHandle_t myTaskHandle2 = NULL;
+#define ESP_INRTERRUPT_FLAG_DEFAULT 0
+#define LED_PIN 27
+#define PUSH_BUTTON_PIN 33
 
-void Demo_Task(void *arg) // Function to be executed by the task
+TaskHandle_t ISR = NULL; //INTERRUPT SERVICE ROUTINE
+
+void IRAM_ATTR button_isr_handler(void* arg) // Interrupt service routine
 {
-    int count = 0;
-    while(1){
-        count++;
-        printf("Demo_Task printing..\n"); 
-        vTaskDelay(1000/ portTICK_PERIOD_MS);
-        if (count == 5)
-        {
-          vTaskSuspend(myTaskHandle2);
-          printf("Demo_Task2 is suspended!\n");
-        }
-        if (count == 8)
-        {
-          vTaskResume(myTaskHandle2);
-          printf("Demo_Task2 is resumed!\n");
-        }
-        if (count == 12)
-        {
-          vTaskDelete(myTaskHandle2);
-          printf("Demo_Task is deleted!\n");
-        }
-    }
+    xTaskResumeFromISR(ISR); // Resume the task from ISR
 }
 
-void Demo_Task2(void *arg)
+void interrupt_task(void* arg) // Task to handle the interrupt
 {
-    for (int i = 0; i < 5; i++) {
-        printf("Demo_Task2 printing..\n"); // Function to be executed by the second task
-        vTaskDelay(1000/ portTICK_PERIOD_MS);
+    bool led_state = false;
+    while(1) {
+        vTaskSuspend(NULL); // Suspend the task until resumed by ISR
+        led_state = !led_state;
+        gpio_set_level(LED_PIN, led_state); // Toggle LED state
+        printf("Button Pressed!\n");
     }
 }
 
 void app_main(void)
 {
-   xTaskCreate(Demo_Task, "Demo_Task", 4096, NULL, 10, &myTaskHandle); // Create the first task
-   xTaskCreatePinnedToCore(Demo_Task2, "Demo_Task2", 4096, NULL,10, &myTaskHandle2, 1);  // Create the second task pinned to core 1
- }
+    // Configure LED pin
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_PIN, 0); // Turn off LED initially
+
+    // Configure push button pin
+    esp_rom_gpio_pad_select_gpio(PUSH_BUTTON_PIN);
+    gpio_set_direction(PUSH_BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(PUSH_BUTTON_PIN, GPIO_PULLUP_ONLY); // Enable internal pull-up resistor
+
+    // Create the interrupt handling task
+    xTaskCreate(interrupt_task, "interrupt_task", 2048, NULL, 10, &ISR);
+
+    // Install ISR service
+    gpio_install_isr_service(ESP_INRTERRUPT_FLAG_DEFAULT);
+    // Attach the interrupt service routine to the button pin
+    gpio_isr_handler_add(PUSH_BUTTON_PIN, button_isr_handler, NULL);
+}
