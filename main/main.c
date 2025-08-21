817e195445e2ca62c9911131e9b7d847a877945c
Alejandro Vivas <alejandrovivash5@gmail.com>
2025-08-21 12:59:29 -0400

    new way

--- main/main.c
+++ main/main.c
@@ -2,27 +2,34 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-TaskHandle_t myTaskHandle = NULL;
+TaskHandle_t myTaskHandle = NULL; 
 TaskHandle_t myTaskHandle2 = NULL;
 
-void Demo_Task(void *arg)
+void Demo_Task(void *arg) // Function to be executed by the task
 {
+    int count = 0;
     while(1){
-        printf("Demo_Task printing..\n");
+        count++;
+        printf("Demo_Task printing..\n"); 
         vTaskDelay(1000/ portTICK_PERIOD_MS);
+        if (count == 5)
+        {
+          vTaskDelete(myTaskHandle2);
+          printf("Demo_Task2 is deleted!\n");
+        }
     }
 }
 
 void Demo_Task2(void *arg)
 {
-    while(1){
-        printf("Demo_Task2 printing..\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Demo_Task2 printing..\n"); // Function to be executed by the second task
         vTaskDelay(1000/ portTICK_PERIOD_MS);
     }
 }
 
 void app_main(void)
 {
-   xTaskCreate(Demo_Task, "Demo_Task", 4096, NULL, 10, &myTaskHandle);
-   xTaskCreatePinnedToCore(Demo_Task2, "Demo_Task2", 4096, NULL,10, &myTaskHandle2, 1);
+   xTaskCreate(Demo_Task, "Demo_Task", 4096, NULL, 10, &myTaskHandle); // Create the first task
+   xTaskCreatePinnedToCore(Demo_Task2, "Demo_Task2", 4096, NULL,10, &myTaskHandle2, 1);  // Create the second task pinned to core 1
  }
